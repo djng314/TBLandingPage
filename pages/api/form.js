845c62ca7664b131cbabeca93ade65a7576dacde
djng314 <64079593+djng314@users.noreply.github.com>
2022-07-01 05:18:52 +0100

    Redirect issue patch #1

--- pages/api/form.js
+++ pages/api/form.js
@@ -3,17 +3,14 @@ import dbConnect from '../../lib/dbConnect';
 import contacts from '../../model/contacts'
 export default async function handler(req, res) {
     await dbConnect()
-    // Get data submitted in request's body.
+
     const body = req.body
   
-    // Optional logging to see the responses
-    // in the command line where next.js app is running.
+
     console.log('body: ', body)
-  
-    // Guard clause checks for first and last name,
-    // and returns early if they are not found
+
     if (!body.preferred || !body.email) {
-      // Sends a HTTP bad request error code
+
       return res.status(400).json({ data: 'First or email name not found' })
     }else{
       await contacts.create({
@@ -22,9 +19,9 @@ export default async function handler(req, res) {
       })
     }
   
-    // Found the name.
-    // Sends a HTTP success code
-    res.redirect('/form1Success',200)
+
+
+    res.writeHead(302, { Location: '/form1Success' }).end()
 
 
   }
